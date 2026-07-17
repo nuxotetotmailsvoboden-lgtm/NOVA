9f679d079da2ac64c79be9871417e23028503067
Name1 <nuxotetotmailsvoboden@gmail.com>
2026-07-18 00:54:52 +0500

    Create routes.ts

---
+++ lib/routes.ts
@@ -0,0 +1,12 @@
+export const ROUTES = {
+  HOME: "/",
+  SERVICES: "/services",
+  BUILDER: "/builder",
+  CALCULATOR: "/calculator",
+  PORTFOLIO: "/portfolio",
+  REVIEWS: "/reviews",
+  FAQ: "/faq",
+  CONTACTS: "/contacts",
+  PROFILE: "/profile",
+  ADMIN: "/admin",
+} as const;
