c8509ede6f9a0db254e24847e517904c4782a11c
Name1 <nuxotetotmailsvoboden@gmail.com>
2026-07-18 00:55:45 +0500

    Create Logo.tsx

---
+++ components/Logo.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function Logo() {
+  return (
+    <div className="nova-logo-wrap">
+      <div className="nova-logo-icon">N</div>
+
+      <div className="nova-logo-text">
+        <h2>NOVA</h2>
+        <span>Premium Automation</span>
+      </div>
+    </div>
+  );
+}
