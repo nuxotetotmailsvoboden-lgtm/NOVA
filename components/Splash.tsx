437f4e4fbd75059bf2976daea28e9a14c00bdcd2
Name1 <nuxotetotmailsvoboden@gmail.com>
2026-07-18 00:33:41 +0500

    Create Splash.tsx

---
+++ components/Splash.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+export default function Splash() {
+  return (
+    <section className="nova-splash">
+      <div className="nova-logo">N</div>
+
+      <h1 className="nova-title">
+        NOVA
+      </h1>
+
+      <p className="nova-subtitle">
+        Premium Telegram Automation
+      </p>
+    </section>
+  );
+}
