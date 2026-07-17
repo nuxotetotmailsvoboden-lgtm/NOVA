8778b66035e2fd7e15366051f18bd39bbc4c71f8
Name1 <nuxotetotmailsvoboden@gmail.com>
2026-07-18 01:20:11 +0500

    Create page.tsx

---
+++ app/dashboard/crm/page.tsx
@@ -0,0 +1,10 @@
+import DashboardLayout from "@/components/dashboard/DashboardLayout";
+import CRMPage from "@/components/crm/CRMPage";
+
+export default function CRM() {
+  return (
+    <DashboardLayout>
+      <CRMPage />
+    </DashboardLayout>
+  );
+}
