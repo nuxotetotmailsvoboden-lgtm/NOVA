6e49fd6a2691aaf0e58fbdb89ef214e46f0d5040
Name1 <nuxotetotmailsvoboden@gmail.com>
2026-07-18 00:59:01 +0500

    Create Aurora.tsx

---
+++ components/background/Aurora.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+export default function Aurora() {
+    return (
+        <div className="aurora">
+
+            <div className="aurora-light aurora-purple"></div>
+
+            <div className="aurora-light aurora-blue"></div>
+
+            <div className="aurora-light aurora-pink"></div>
+
+        </div>
+    );
+}
