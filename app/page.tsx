db435fb1894996d2e330f0e6980ce5748eeef9bd
Name1 <nuxotetotmailsvoboden@gmail.com>
2026-07-18 01:03:40 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Layout from "@/components/Layout";
 import Hero from "@/components/Hero";
+import Services from "@/components/sections/Services";
 
 export default function Home() {
   return (
     <Layout>
       <Hero />
+      <Services />
     </Layout>
   );
 }
