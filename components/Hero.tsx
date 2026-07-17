9fc87a12070ddb4fb13f00c6721417ab5ded9d3a
Name1 <nuxotetotmailsvoboden@gmail.com>
2026-07-18 00:46:51 +0500

    Create Hero.tsx

---
+++ components/Hero.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+export default function Hero() {
+  return (
+    <main className="hero">
+
+      <div className="hero-blur hero-blur-1"></div>
+      <div className="hero-blur hero-blur-2"></div>
+
+      <section className="hero-card">
+
+        <span className="hero-badge">
+          🚀 Premium Platform
+        </span>
+
+        <h1>
+          Build your
+          <br />
+          Telegram Business
+        </h1>
+
+        <p>
+          Design premium Telegram bots,
+          Mini Apps and automations
+          with NOVA.
+        </p>
+
+        <div className="hero-buttons">
+
+          <button className="primary">
+            Start Building
+          </button>
+
+          <button className="secondary">
+            Watch Demo
+          </button>
+
+        </div>
+
+      </section>
+
+    </main>
+  );
+}
