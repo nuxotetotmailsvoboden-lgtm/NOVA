b880ba07804a5062d38d3a691a62ad8be7d52811
Name1 <nuxotetotmailsvoboden@gmail.com>
2026-07-18 00:59:25 +0500

    Create Background.tsx

---
+++ components/background/Background.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Aurora from "./Aurora";
+import Grid from "./Grid";
+
+export default function Background() {
+    return (
+        <>
+            <Aurora />
+            <Grid />
+        </>
+    );
+}
